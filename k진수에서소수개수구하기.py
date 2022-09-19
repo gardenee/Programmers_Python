12d6208624a0e41786fc6149c9fb737f034058bf
gardenee <snnjjang@naver.com>
2022-09-19 22:15:35 +0900

    220919

---
+++ k진수에서소수개수구하기.py
@@ -0,0 +1,38 @@
+def change(n, k):
+    i = 0
+    while True:
+        if k ** (i + 1) > n:
+            break
+        i += 1
+
+    result = ""
+    for j in range(i, -1, -1):
+        mul = k ** j
+        result += str(n // mul)
+        n %= mul
+
+    return result
+
+
+def solution(n, k):
+    answer = 0
+
+    n = change(n, k)
+    n = n.replace("0", " ")
+    nums = list(map(int, n.split()))
+    print("nums", nums)
+
+    for num in nums:
+        if num != 1:
+            temp = 1
+        else:
+            temp = 0
+
+        for i in range(2, int(round(num ** 0.5, 0)) + 1):
+            if num % i == 0:
+                temp = 0
+                break
+        print("num", num, "temp", temp)
+        answer += temp
+
+    return answer
