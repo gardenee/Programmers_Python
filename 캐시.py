2ebf210699c6797c0e8503dd2701544d7b7a19de
gardenee <snnjjang@naver.com>
2022-09-23 00:37:17 +0900

    220923

---
+++ 캐시.py
@@ -0,0 +1,16 @@
+def solution(cacheSize, cities):
+    answer = 0
+    cache = []
+    for city in cities:
+        city = city.upper()
+        if city in cache:
+            answer += 1
+            cache.pop(cache.index(city))
+            cache.append(city)
+        else:
+            answer += 5
+            cache.append(city)
+            if len(cache) > cacheSize:
+                cache.pop(0)
+
+    return answer
