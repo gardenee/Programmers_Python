fbfb6861f110aab463b9f4300ce294f8fc9760ab
gardenee <snnjjang@naver.com>
2022-10-01 22:13:35 +0900

    221001

---
+++ 구명보트.py
@@ -0,0 +1,14 @@
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    i, j = 0, len(people) - 1
+    while i <= j:
+        if people[j] + people[i] > limit:
+            answer += 1
+            j -= 1
+        else:
+            i += 1
+            j -= 1
+            answer += 1
+
+    return answer
